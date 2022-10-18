ebc764c63945a89ef56ea9ee1f4b07406b1976df
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2022-10-18 15:33:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (99.94%)

--- my-solutions/1791-richest-customer-wealth/solution.java
+++ my-solutions/1791-richest-customer-wealth/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        int s = 0;
-        int c= 0;
-        for (int[] i : accounts){
-            for(int j :i){
-                c+=j;
-            }
-            if(c>s){
-                s=c;
-            }
-            c=0;
+        int m = Integer.MIN_VALUE;
+        for(int[] i:accounts){
+            int n = sum(i);
+            if(n>m) m=n;
+        }
+        return m;
+    }
+    int sum(int[] arr){
+        int s =0;
+        for(int i:arr){
+            s+=i;
         }
         return s;
-        
     }
 }
