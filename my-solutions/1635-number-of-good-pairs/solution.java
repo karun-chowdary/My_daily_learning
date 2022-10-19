b9b4b5bd3824e4389241ea7c6bf5eac53599cd3c
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2022-10-19 17:15:44 +0000

    Sync LeetCode submission Runtime - 1 ms (84.60%), Memory - 39.7 MB (100.00%)

--- my-solutions/1635-number-of-good-pairs/solution.java
+++ my-solutions/1635-number-of-good-pairs/solution.java
@@ -1,21 +1,13 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-      //  return goodPairs(nums);
-        int ans = 0;
-        int[] count = new int[101];
-        
-        for(int n: nums)
-            count[n]++;
-        
-        for(int n: count)
-            ans += (n * (n - 1))/2;
-        
-        return ans;
+        int n = nums.length;
+        int c =0;
+        for(int i =0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]) c+=1;
+            }
+        }
+        return c;
         
     }
- /*   static int goodPairs(int[] a){
-        int c =0;
-        for (int i=0;i<a.length;i++) for(int j=i+1;j<a.length;j++) if(a[i]==a[j]) c+=1;       
-        return c;
-    }*/
 }
