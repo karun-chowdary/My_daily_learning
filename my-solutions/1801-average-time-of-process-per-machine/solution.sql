2603ab938dd5070d518d7e02fe81ec7bd1e14969
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-01-27 13:27:15 +0000

    Sync LeetCode submission Runtime - 454 ms (5.56%), Memory - 0.0B (100.00%)

--- my-solutions/1801-average-time-of-process-per-machine/solution.sql
+++ my-solutions/1801-average-time-of-process-per-machine/solution.sql
@@ -3,6 +3,6 @@ SELECT
     s.machine_id, 
     ROUND(AVG(e.timestamp-s.timestamp),3) as processing_time 
 FROM Activity s
-JOIN Activity e ON e.machine_id=s.machine_id
+JOIN Activity e ON s.machine_id=e.machine_id
 WHERE s.activity_type='start' AND e.activity_type='end'
 GROUP BY s.machine_id
