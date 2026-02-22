9672213de0cc484dd1a50468123fd9814dd5591b
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-02-22 10:00:51 +0530

    Time: 552 ms (73.75%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -8,3 +8,12 @@ SUM((state = "approved")* amount) as approved_total_amount
 FROM transactions
 GROUP BY month, country
 
+#pyspark query
+/* from pyspark.sql import functions as F
+results = transactions.groupBy(F.substring(trans_date,1,7).alias("month"), country)
+.agg(
+    F.count(id).alias("trans_count"),
+    F.sum(F.when(F.col('state')=="approved"),1).otherwise(0).alias('approved_count'),
+    F.sum(F.col("amount").alias('trans_total_amount'),
+    F.sum(F.when(F.col('state')=='approved',F.col('amount')),1).otherwise(0)).alias('approved_total_amount')
+)*/
