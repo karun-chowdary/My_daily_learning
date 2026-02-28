3877e2d5ccb6578a648580d69a38c8502d755c3f
Karun Sai Chowdary <109739413+karun-chowdary@users.noreply.github.com>
2026-02-28 17:49:38 +0000

    Sync LeetCode submission Runtime - 368 ms (22.13%), Memory - 0.0B (100.00%)

--- my-solutions/0626-exchange-seats/solution.sql
+++ my-solutions/0626-exchange-seats/solution.sql
@@ -16,3 +16,13 @@ END,
  student) AS student
 FROM seat
 
+#pysaprk query
+/*from pyspark.sql.window import Window
+
+window_spec = Window.orderBy() # Empty partition = whole table
+result = seat.withColumn("max_id", F.max("id").over(window_spec)) \
+             .withColumn("id", 
+                 F.when((F.col("id") % 2 == 1) & (F.col("id") < F.col("max_id")), F.col("id") + 1)
+                  .when(F.col("id") % 2 == 0, F.col("id") - 1)
+                  .otherwise(F.col("id"))
+             ).drop("max_id").orderBy("id")*/
